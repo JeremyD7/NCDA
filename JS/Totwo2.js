a21a4cd65bba8b9d8246bdf5289a484a0f2f701d
Jeremy_D <1418776145@qq.com>
2025-06-05 23:26:18 +0800

    fix:bug

--- JS/Totwo2.js
+++ JS/Totwo2.js
@@ -4,17 +4,17 @@ const nav = document.querySelector('.header__nav ul')
 nav.addEventListener('click', function (e) {
   e.preventDefault()
   if (e.target.tagName === 'A' && e.target.dataset.id === '2') {
-    location.href = "html/web-1920-2.html"
+    location.href = "./web-1920-2.html"
     // console.log('yes');
   }
 
   else if (e.target.tagName === 'A' && e.target.dataset.id === '3') {
-    location.href = "html/web-1920-3.html"
+    location.href = "./web-1920-3.html"
   }
   // console.log(e.target);
 
   else if (e.target.tagName === 'A' && e.target.dataset.id === '4') {
-    location.href = "html/web-1920-8.html"
+    location.href = "./web-1920-8.html"
   }
   else {
     location.href = "../index.html"
