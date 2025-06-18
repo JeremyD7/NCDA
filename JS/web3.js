290cf7251eb33efc70768c76abb6023f86049b61
Jeremy_D <1418776145@qq.com>
2025-06-18 19:53:05 +0800

    end

--- JS/web3.js
+++ JS/web3.js
@@ -1,8 +1,6 @@
 const button = document.querySelector('.header__nav ul li:nth-child(3) a');
 const introduce = document.querySelector('.introduce')
 button.addEventListener('click', function (e) {
-
   e.preventDefault()
-
   introduce.style.display = 'flex';
 })
